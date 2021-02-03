24b26967b3bc7674da96552ad5fa77d176845f51
Franzen <jean.franzen@hotmail.com>
2021-02-03 13:44:37 -0300

    Retornar 404 caso não exista a categoria

--- src/main/java/com/example/algamoneyapi/repository/CategoriaRepository.java
+++ src/main/java/com/example/algamoneyapi/repository/CategoriaRepository.java
@@ -1,9 +1,12 @@
 package com.example.algamoneyapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoneyapi.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
+	
 }
--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,22 @@
 package com.example.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.repository.CategoriaRepository;
@@ -21,7 +32,33 @@ public class CategoriaResource {
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
 	}
+//	public ResponseEntity<?> listar(){
+//		List<Categoria> categorias = categoriaRepository.findAll();
+//		return  !categorias.isEmpty()? ResponseEntity.ok(categorias): ResponseEntity.noContent().build();
 	
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		//Pegar a url atual e o codigo do objeto que foi inserido no banco de dados
+		URI uri =ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		
+		//Resposta para o header da requisição
+		response.setHeader("Location", uri.toASCIIString());
+		
+		//retorna o status e o body do objeto
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categorias = categoriaRepository.findById(codigo);
+		return  categorias.isPresent()? 
+				ResponseEntity.ok(categorias.get()) : ResponseEntity.notFound().build();
+		
+	}
 	
 
 }
